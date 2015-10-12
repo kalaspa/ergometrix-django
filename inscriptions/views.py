a984d20536a7ecb784bd9b971814238db971fd67
Akamine <kalaspa@gmail.com>
2015-10-12 23:21:15 +0200

    Premieres fonctions accessibles par l'exterieur

--- inscriptions/views.py
+++ inscriptions/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route, list_route
+from rest_framework.response import Response
+from rest_framework import status
+
 from inscriptions.models import Boat , Rower , Leader , Category
 from inscriptions.serializers import BoatSerializer , RowerSerializer , LeaderSerializer , CategorySerialier
 # Create your views here.
@@ -7,6 +11,32 @@ from inscriptions.serializers import BoatSerializer , RowerSerializer , LeaderSe
 class BoatViewSet (viewsets.ModelViewSet):
 	queryset = Boat.objects.all()
 	serializer_class = BoatSerializer
+
+	@detail_route(methods=['post'])
+	def pay(self, request, pk=None):
+
+		try:
+			boat = Boat.objects.get(id=pk)
+		except Snippet.DoesNotExist:
+			return HttpResponse(status=404)
+
+		boat.payment = True
+		boat.save()
+		return Response({'status': 'payment set'})
+
+	@detail_route(methods=['post'])
+	def valid(self, request, pk=None):
+
+		try:
+			boat = Boat.objects.get(id=pk)
+		except Snippet.DoesNotExist:
+			return HttpResponse(status=404)
+
+		boat.valid = True
+		boat.save()
+		return Response({'status': 'validation set'})
+
+
 
 class RowerViewSet (viewsets.ModelViewSet):
 	queryset = Rower.objects.all()
